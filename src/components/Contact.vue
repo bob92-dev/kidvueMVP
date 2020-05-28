a2439cc726939b25724c7c5acf3cf4bc9ba41e72
Romain <romain.dupuis@edu.itescia.fr>
2020-05-28 15:13:36 +0200

    contact

---
+++ src/components/Contact.vue
@@ -0,0 +1,28 @@
+<template>
+  <form class="contact-form" @submit.prevent="sendEmail">
+    <label>Name</label>
+    <input type="text" name="user_name">
+    <label>Email</label>
+    <input type="email" name="user_email">
+    <label>Message</label>
+    <textarea name="message"></textarea>
+    <input type="submit" value="Send">
+  </form>
+</template>
+
+<script>
+import emailjs from 'emailjs-com';
+
+export default {
+  methods: {
+    sendEmail: (e) => {
+      emailjs.sendForm('romain', 'template_9oGD6HyL', e.target, 'user_4Epjr97MQiSODRf4hYtlP')
+        .then((result) => {
+            console.log('SUCCESS!', result.status, result.text);
+        }, (error) => {
+            console.log('FAILED...', error);
+        });
+    }
+  }
+}
+</script>
