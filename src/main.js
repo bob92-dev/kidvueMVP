5de4e678f2292e0a3d885dcc7bc00501802bcd8d
Tania <tania.rojas@edu.itescia.fr>
2020-06-02 17:06:13 +0200

    fb config

--- src/main.js
+++ src/main.js
@@ -4,7 +4,19 @@ import router from "./router";
 
 Vue.config.productionTip = false;
 
+const firebaseConfig = {
+    apiKey: "AIzaSyBkQlrXVX1ZMP4ynr-lXHOPYzlxuslzewo",
+    authDomain: "kidvuemvp.firebaseapp.com",
+    databaseURL: "https://kidvuemvp.firebaseio.com/",
+    projectId: "kidvuemvp",
+    storageBucket: "kidvuemvp.appspot.com",
+    messagingSenderId: "8208204539",
+    appId: "1:8208204539:web:ab248e19d14797e935265e",
+    measurementId: "G-WXW80KT5WX"
+};
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount("#app");
