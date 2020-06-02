f839ef9adfbd3ebd2adc155bf94d41c8b565eac9
Romain <romain.dupuis@edu.itescia.fr>
2020-06-02 17:01:58 +0200

    debut inscription

--- src/views/Inscription.vue
+++ src/views/Inscription.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        page inscription
+    <div class="container">
+        <h1>Inscription</h1>
+        <form>
+            <div class ="form-group">
+                <label for="email"> E-mail</label>
+                <input type="email" id="mail" class="form-control">
+            </div>
+
+             <div class ="form-group">
+                <label for="password"> Mot de passe</label>
+                <input type="password" id="password" class="form-control">
+            </div>
+
+             <div class ="form-group">
+                <label for="text"> Confirmez le mot de passe</label>
+                <input type="password" id="conf-password" class="form-control">
+            </div>
+
+            
+        </form>
     </div>
 </template>
 
 <script>
+
 export default {
     
 }
 </script>
 
 <style scoped>
-
+.container {
+    margin-top: 10;
+  position: relative;
+  width: 45%;
+}
 </style>
