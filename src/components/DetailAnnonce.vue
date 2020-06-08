0ed14c70e13e4e9313a4b6367f8d18de078f3ff7
cassel boris <casselboris92@gmail.com>
2020-06-08 21:32:29 +0200

    algo firebase de tania ajouté

--- src/components/DetailAnnonce.vue
+++ src/components/DetailAnnonce.vue
@@ -9,24 +9,78 @@
 </template>
 <script>
 
-// import firebase from 'firebase';
+import firebase from 'firebase';
 export default {
     props : ['id'],
     data(){
         return {
             annonce : {}
         }
+    },
+     created:function(){
+        this.recupData()
+      },
+    methods:{
+      recupData(){
+        console.log("coucou id " + this.id);
+        var refAnnonce = firebase.database().ref('/annonces/' +this.id);
+        console.log(refAnnonce)
+        var titre;
+        refAnnonce.child("titre").on("value",function(snapshot){
+          titre=snapshot.val;
+          console.log(titre);
+        })
+        var categorie;
+        refAnnonce.child("categorie").on("value",function(snapshot){
+          categorie=snapshot.val;
+          console.log(categorie);
+        })
+        var description;
+        refAnnonce.child("description").on("value",function(snapshot){
+          description=snapshot.val;
+          console.log(description);
+        })
+        var marque;
+        refAnnonce.child("marque").on("value",function(snapshot){
+          marque=snapshot.val;
+          console.log(marque);
+        })
+        var img;
+        refAnnonce.child("imageUrl").on("value",function(snapshot){
+          img=snapshot.val;
+          console.log(img);
+        })
+        var prix;
+        refAnnonce.child("prix").on("value",function(snapshot){
+          prix=snapshot.val;
+          console.log(prix);
+        })
+        var ville;
+        refAnnonce.child("ville").on("value",function(snapshot){
+          ville=snapshot.val;
+          console.log(ville);
+        })
+        var user;
+        var pseudo;
+        refAnnonce.child("propriétaire").on("value",function(snapshot){
+          user=snapshot.val;
+          let ref = firebase.database().ref('users/' + user).child("pseudo");
+          ref.on("value", function(snapshot) {    
+            pseudo =snapshot.val();
+            console.log(pseudo)
+          });
+        })
+        var datePublication;
+        refAnnonce.child("dateDePublication").on("value",function(snapshot){
+          datePublication=snapshot.val;
+          console.log(datePublication);
+        })
+        var dateIndisponibilite;
+        refAnnonce.child("dateIndisponibilité").on("value",function(snapshot){
+          dateIndisponibilite=snapshot.val;
+          console.log(dateIndisponibilite);
+        })
+      }
     }      
-        
-        /* 
-        this.refdb.once('value', function(snapshot) {
-            snapshot.forEach(function(childSnapshot) {
-                var childKey = childSnapshot.key;
-                var childData = childSnapshot.val();
-                // ...
-                console.log(childKey + " " + childData)
-            });
-        }) */
-    
-    } 
+}      
 </script>
