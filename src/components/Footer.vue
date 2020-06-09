619cd65e0269483ec3856c717c3fe9759c6ea6d9
cassel boris <casselboris92@gmail.com>
2020-06-09 09:49:50 +0200

    footer et inscription

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,12 +9,10 @@
 
       <v-btn color="white" text rounded class="my-2" to="/apropos">QUI SOMMES-NOUS ?</v-btn>
 
-      <v-btn color="white" text rounded class="my-2" to="/contact"></v-btn>
 
 
       <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-        {{ new Date().getFullYear() }} —
-        <strong>KidsOnTrip</strong>
+        {{ new Date().getFullYear() }} — <strong>KidsOnTrip</strong>
       </v-col>
     </v-row>
   </v-footer>
--- src/components/Inscription.vue
+++ src/components/Inscription.vue
@@ -67,39 +67,37 @@
       </div>
     </div>
 
-<v-card width="400" class="mx-auto mt-5">
-			<v-card-title>
-				<h1 class="display-1">Login</h1>
-			</v-card-title>
-			<v-card-text>
-				<v-form>
-					<v-text-field label="Username" prepend-icon="mdi-account-circle" />
-					<v-text-field
-						:type="showPassword ? 'text' : 'password'"
-						label="Password"
-						prepend-icon="mdi-lock"
-						:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-						@click:append="showPassword = !showPassword"
-					/>
-				</v-form>
-			</v-card-text>
-			<v-divider></v-divider>
-			<v-card-actions>
-				<v-btn color="success">Register</v-btn>
-				<v-spacer></v-spacer>
-				<v-btn color="info">Login</v-btn>
-			</v-card-actions>
-		</v-card>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="display-1">Login</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="Username" prepend-icon="mdi-account-circle" />
+          <v-text-field
+            :type="showPassword ? 'text' : 'password'"
+            label="Password"
+            prepend-icon="mdi-lock"
+            :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+            @click:append="showPassword = !showPassword"
+          />
+        </v-form>
+      </v-card-text>
+      <v-divider></v-divider>
+      <v-card-actions>
+        <v-btn color="success">Register</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="info">Login</v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
 
 <script>
 import firebase from "firebase";
 
-
 export default {
   data() {
-    
     return {
       connected: this.$store.getters.loggedIn,
       form: {
@@ -121,11 +119,9 @@ export default {
               displayName: this.form.name
             })
             .then(() => {
-             this.$router.replace({ 
-               name: "profil" 
-               });
-
-
+              this.$router.replace({
+                name: "profil"
+              });
             });
         })
         .catch(err => {
